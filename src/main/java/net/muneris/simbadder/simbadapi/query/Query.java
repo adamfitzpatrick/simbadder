f676f92cc3e3292b30599dadacf662ed8fc22796
adamfitz77hob <adam.fitzpatrick@hobsons.com>
2015-07-20 21:45:45 -0700

    S-7 add custom query

---
+++ src/main/java/net/muneris/simbadder/simbadapi/query/Query.java
@@ -0,0 +1,6 @@
+package net.muneris.simbadder.simbadapi.query;
+
+public interface Query {
+
+	public String getQueryString();
+}
---
+++ src/test/java/net/muneris/simbadder/simbadapi/query/CustomQueryTest.java
@@ -0,0 +1,33 @@
+package net.muneris.simbadder.simbadapi.query;
+
+import static net.muneris.simbadder.testUtils.TestConstants.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomQueryTest {
+
+	CustomQuery query;
+	
+	@Before
+	public void setUp() throws Exception {
+		query = new CustomQuery(STRING);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCustomQuery() {
+		assertNotNull(query);
+	}
+
+	@Test
+	public void testGetQueryString() {
+		assertEquals(STRING, query.getQueryString());
+	}
+
+}
