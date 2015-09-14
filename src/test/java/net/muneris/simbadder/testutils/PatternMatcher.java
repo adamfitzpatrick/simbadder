f68342871fd28c5b30ef9b1251b7c2fbb82491c2
adamfitz77hob <adam.fitzpatrick@hobsons.com>
2015-09-14 16:06:05 -0700

    Add unit test for exceptions and LoggingFilter

---
+++ src/test/java/net/muneris/simbadder/testutils/PatternMatcher.java
@@ -0,0 +1,32 @@
+package net.muneris.simbadder.testutils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class PatternMatcher extends TypeSafeMatcher<String> {
+
+    private final Pattern pattern;
+    private Matcher matcher;
+    
+    public static PatternMatcher matches(String pattern) {
+        return new PatternMatcher(pattern);
+    }
+    
+    public PatternMatcher(String patternStr) {
+        this.pattern = Pattern.compile(patternStr);
+    }
+    
+    @Override
+    public void describeTo(Description description) {
+        // Not Implemented
+    }
+
+    @Override
+    protected boolean matchesSafely(String item) {
+        matcher = pattern.matcher(item);
+        return matcher.find(0);
+    }
+}
